129a092f0aea4c6ed2ac88d21ddface29dd613c9
Jen <jen.frazier1@gmail.com>
2020-04-06 14:43:54 -0500

    for checkpoint-18

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,12 +1,10 @@
-{
+
   class Helper{
 playPauseAndUpdate (song) {
   player.playPause(song);
-  const duration = Player.getDuration();
+  const duration = player.getDuration();
   $('#time-control .total-time').text( duration );
 }
 }
 
 const helper = new Helper();
-
-}
